1c0c5fc0b7cda7d0e025daf109be631493b051c3
Unknown <sergaliyeva@mail.ru>
2018-10-29 08:08:13 +0300

    правки в главном файле + вызов mystem

--- project1/main.py
+++ project1/main.py
@@ -1,11 +1,15 @@
 import articles
 import dirs
+import analyse
 
-common_url = 'http://www.zarpressa.ru'     # страница сайта
-pages_limit = 5                   # количество статей для посещения
+common_url = 'http://www.zarpressa.ru'      # страница сайта
+pages_limit = 250                           # количество статей для посещения
 
 # инициализация структуры директории для сохранения всех статей и результатов анализов
 dirs.init_dirs()
 
 # сначала делается поиск и загрузка страниц со статьями
 articles.connect(common_url, pages_limit)
+
+# анализ через mystem
+analyse.write_mystem_xml()
