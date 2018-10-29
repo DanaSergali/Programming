87a7ea14f4acdfe0412cbeb9615db709c49409a8
Unknown <sergaliyeva@mail.ru>
2018-10-29 04:20:33 +0300

    ошибки в создании папок

--- project1/dirs.py
+++ project1/dirs.py
@@ -13,8 +13,15 @@ def init_dirs():
 
 # создание папок по дате для статьи и ее анализа
 def create_dir_by_date(year, month):
-    year_str = year.str()
-    month_str = month.str()
-    os.makedirs('newspaper/plain/%s/%s' % (year_str, month_str))
-    os.makedirs('newspaper/mystem-xml/%s/%s' % (year_str, month_str))
-    os.makedirs('newspaper/mystem-plain/%s/%s' % (year_str, month_str))
+    plain = 'newspaper/plain/%s/%s' % (year, month)
+    mystem_xml = 'newspaper/mystem-xml/%s/%s' % (year, month)
+    mystem_plain = 'newspaper/mystem-plain/%s/%s' % (year, month)
+
+    if not os.path.exists(plain):
+        os.makedirs(plain)
+
+    if not os.path.exists(mystem_xml):
+        os.makedirs(mystem_xml)
+
+    if not os.path.exists(mystem_plain):
+        os.makedirs(mystem_plain)
